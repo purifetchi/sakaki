1b2db8450b812a285e2fd63b43603dac245bc136
naomiEve <stapa2000@gmail.com>
2017-09-17 01:41:50 +0200

    sql

--- src/app.rb
+++ src/app.rb
@@ -1,11 +1,23 @@
+# app.rb - Sakaki Renderer
+# (c) 2017 prefetcher
+
 require 'gopher2000'
 require 'json'
+require 'mysql2'
 
 # Config loading & host:port setup
 
 config = JSON.parse(File.read('config.json'))
 set :host, config["host_ip"]
 set :port, config["port"]
+
+def make_con()
+  return Mysql2::Client.new(:host => "localhost", :username => "sakaki", :password => "sakaki", :database => "sakaki")
+end
+
+def query(con, stmt, *args)
+  return con.prepare(stmt).execute(*args)
+end
 
 # Code for Sakaki routing begins here
 route '/' do
@@ -14,7 +26,7 @@ end
 
 config["boards"].each do |board, array|
   route '/' + board do
-    render :board
+    render :board, make_con(), board
   end
 end
 
@@ -28,6 +40,8 @@ menu :index do
   br
 end
 
-menu :board do
-  text "board"
+menu :board do |con, board|
+  query(con, "SELECT * WHERE board=#{board} AND is_op = 1 ORDER BY bump_date DESC") do |res|
+    link "=#{res["title"]}= [Created on: #{res["date_posted"]}, Latest bump on: #{res["bump_date"]}]"
+  end
 end
