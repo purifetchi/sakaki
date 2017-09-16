c27e725122ad04a4d9b8360dcdd385f32f8fe68e
naomiEve <stapa2000@gmail.com>
2017-09-17 00:52:27 +0200

    boards load:

--- src/app.rb
+++ src/app.rb
@@ -1,11 +1,33 @@
 require 'gopher2000'
+require 'json'
 
-set :host, '0.0.0.0'
+# Config loading & host:port setup
 
+config = JSON.parse(File.read('config.json'))
+set :host, config["host_ip"]
+set :port, config["port"]
+
+# Code for Sakaki routing begins here
 route '/' do
   render :index
 end
 
+config["boards"].each do |board, array|
+  route '/' + board do
+    render :board
+  end
+end
+
+# All the renderable menus
 menu :index do
-  text "index"
+  big_header "Sakaki"
+  br
+  config["boards"].each do |board, array|
+    link "/#{board}/ - #{config["boards"][board]["description"]}", "/#{board}"
+  end
+  br
+end
+
+menu :board do
+  text "board"
 end
